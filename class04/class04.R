92dc6b7e698552f3f429a238a8034e95794ccd39
sion kang <sik001@ucsd.edu>
2025-02-25 11:41:37 -0800

    add some initial class content

---
+++ class04/class04.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "Class 4: Intro to R"
+#' author: Sion Kang (PID A17052234)
+#' ---
+
+
+x <- 1:50
+plot(x, sin(x))
+
+#make this nicer
+
+plot(x, sin(x), col="blue", type="l", lwd=3)
